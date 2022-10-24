8acfdacc12a47d164be807b9dbbc96aaf69c0fd6
Jan Dolenc <jan.dolenc9@gmail.com>
2022-10-24 18:20:49 +0200

    delete unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { ItemAssetComponent } from './components/item-asset/item-asset.component';
-import { ChartComponent } from './components/chart/chart.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    SidebarComponent,
-    ItemAssetComponent,
-    ChartComponent,
-    DashboardComponent,
-  ],
+  declarations: [AppComponent, DashboardComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
