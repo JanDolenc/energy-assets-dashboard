558e3762b03c854d79539a1b60ff280848e2fb3c
Jan Dolenc <jan.dolenc9@gmail.com>
2022-10-24 23:06:24 +0200

    plot chart with dynamic data WIP

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -24,7 +24,8 @@ export class DashboardComponent implements OnInit {
 
   onSelect(asset: Asset): void {
     this.selectedAsset = asset;
-    this.getAssetData(asset);
+    /* this.getAssetData(asset) */
+    this.plotChart(asset);
   }
 
   getAssetList(): void {
@@ -39,4 +40,115 @@ export class DashboardComponent implements OnInit {
       .getAssetData(asset)
       .subscribe((data) => (this.chartData = data));
   }
+
+  /* CHART */
+  async plotChart(asset: Asset) {
+    this.getAssetData(asset);
+    /* console.log(data); */
+    console.log('aaaa', this.chartData?.data);
+  }
+
+  chartOption: EChartsOption = {
+    title: {
+      text: 'Chart',
+      left: '1%',
+    },
+    tooltip: {
+      trigger: 'axis',
+    },
+    grid: {
+      left: '5%',
+      right: '15%',
+      bottom: '10%',
+    },
+    xAxis: {
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {},
+    toolbox: {
+      right: 10,
+      feature: {
+        dataZoom: {
+          yAxisIndex: 'none',
+        },
+        restore: {},
+        saveAsImage: {},
+      },
+    },
+    dataZoom: [
+      {
+        startValue: '2014-06-01',
+      },
+      {
+        type: 'inside',
+      },
+    ],
+    visualMap: {
+      top: 50,
+      right: 10,
+      pieces: [
+        {
+          gt: 0,
+          lte: 50,
+          color: '#93CE07',
+        },
+        {
+          gt: 50,
+          lte: 100,
+          color: '#FBDB0F',
+        },
+        {
+          gt: 100,
+          lte: 150,
+          color: '#FC7D02',
+        },
+        {
+          gt: 150,
+          lte: 200,
+          color: '#FD0100',
+        },
+        {
+          gt: 200,
+          lte: 300,
+          color: '#AA069F',
+        },
+        {
+          gt: 300,
+          color: '#AC3B2A',
+        },
+      ],
+      outOfRange: {
+        color: '#999',
+      },
+    },
+    series: {
+      name: 'Beijing AQI',
+      type: 'line',
+      data: [820, 932, 901, 934, 1290, 1330, 1320],
+      markLine: {
+        silent: true,
+        lineStyle: {
+          color: '#333',
+        },
+        data: [
+          {
+            yAxis: 50,
+          },
+          {
+            yAxis: 100,
+          },
+          {
+            yAxis: 150,
+          },
+          {
+            yAxis: 200,
+          },
+          {
+            yAxis: 300,
+          },
+        ],
+      },
+    },
+  };
+  /* CHART END */
 }
